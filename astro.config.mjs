749241403008ec060e427e2b956dac0fe66f2be3
putcut <putcutpoint@gmail.com>
2025-02-18 20:32:48 +0900

    astro version update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blol.putcut.net',
+  site: 'https://blog.putcut.net',
   integrations: [mdx(), sitemap(), tailwind()]
 });
